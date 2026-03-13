8670c6754dd3889ed2b83930c35b071a213b1bdb
deeoak sharma <demondark7678@gmail.com>
2026-03-13 20:16:02 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { AuthProvider } from "@/lib/auth";
 import { Toaster } from "react-hot-toast";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
